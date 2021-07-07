6e8b8ceb169e3a7bbe2e913a71bfe9ceabad8a9e
Yunjin Bak <yunjin.bak1@gmail.com>
2021-07-06 20:12:17 -0700

    Template

---
+++ houseprices/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict sales prices and practice feature engineering, RFs, and gradient boosting',
+    author='DW',
+    license='',
+)
